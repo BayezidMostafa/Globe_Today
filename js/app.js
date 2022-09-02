826680471d004e8647346902042005b20862786b
BayezidMostafa <hiperx1@gmail.com>
2022-09-03 00:43:50 +0600

    Dynamic Category Panel Added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-console.log('connected')
+const loadCategory = () => {
+    const url = `https://openapi.programming-hero.com/api/news/categories`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayCatagory(data.data.news_category))
+}
+
+const displayCatagory = categories => {
+    const categoriesContainer = document.getElementById('category-container');
+    categories.forEach(category => {
+        console.log(category)
+        const catDiv = document.createElement('div');
+        catDiv.innerHTML = `
+        <a onclick="gettingCategoryDetails(${category.category_id})" type="button" class="text-decoration-none text-dark">${category.category_name}</a>
+        `;
+        categoriesContainer.appendChild(catDiv);
+    });
+}
+
+
+loadCategory()
