7a667fa4a1a6ed87435c959b0847a8b499ab7f0e
BayezidMostafa <hiperx1@gmail.com>
2022-09-04 04:33:14 +0600

    Sort Out All News And Blog Page Added

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const loadCategory = () => {
     fetch(url)
         .then(res => res.json())
         .then(data => displayCatagory(data.data.news_category))
+        .catch((error) => console.log(error))
 }
 
 const displayCatagory = categories => {
@@ -12,7 +13,7 @@ const displayCatagory = categories => {
         const categoryUl = document.createElement('ul');
         categoryUl.classList.add('navbar-nav');
         categoryUl.innerHTML = `
-        <li class="nav-item">
+        <li class="nav-item me-5">
             <a onclick="gettingCategoryDetails(${category.category_id})" class="nav-link active" aria-current="page" href="#">${category.category_name}</a>
         </li>
         `;
@@ -20,57 +21,59 @@ const displayCatagory = categories => {
     });
 }
 
-
 const gettingCategoryDetails = categoryId => {
     const url = `https://openapi.programming-hero.com/api/news/category/0${categoryId}`;
     fetch(url)
-        .then(res => res.json())
-        .then(data => displayCategoryNews(data.data))
-    toggleingSpinner(true)
+    .then(res => res.json())
+    .then(data => displayCategoryNews(data.data));
+    toggleingSpinner(true);
 }
 
-const displayCategoryNews = newses => {
+const displayCategoryNews = allNewses => {
+    allNewses.sort((view1, view2) => view2.total_view - view1.total_view);
+    const counter = document.getElementById('counter');
     const newsContainer = document.getElementById('news-container');
+    counter.innerText='0';
     newsContainer.innerHTML = ``;
-    newses.forEach(news => {
+    allNewses.forEach(news => {
         // console.log(news)
         const newsDiv = document.createElement('div');
         newsDiv.classList.add('col');
         newsDiv.innerHTML = `
-        <div class="card">
-                <img src="${news.thumbnail_url}" class="card-img-top" alt="...">
+        <div class="card shadow rounded-4">
+                <img src="${news.thumbnail_url}" class="card-img-top p-3" alt="...">
             <div class="card-body">
-                <h5 class="card-title">${news.title}</h5>
-                <p class="card-text">${news.details.slice(0, 500)}...</p>
+                <h5 class="card-title">${news.title ? news.title : 'No title Found'}</h5>
+                <p class="card-text">${news.details.slice(0, 500) ? news.details.slice(0, 500) : 'Informations Missing'}...</p>
                 <div class="d-flex justify-content-between">
-                    <div class="d-flex">
-                        <img style="width: 50px;" class="img-fluid rounded-circle" src="${news.author.img}">
+                    <div class="d-md-flex">
+                        <img style="width: 50px;" class="img-fluid rounded-circle" src="${news.author.img ? news.author.img : 'No Author Image'}">
                         <div class="ms-2">
-                            <p class="m-0">${news.author.name}</p>
-                            <p class="m-0">${news.author.published_date}</p>
+                            <p class="m-0 fw-semibold">${news.author.name ? news.author.name : 'Author Name Missing'}</p>
+                            <p class="m-0 text-muted">${news.author.published_date ? news.author.published_date : 'No Date Found'}</p>
                         </div>
                     </div>
                     <div>
-                        <div class="d-flex">
-                            <div class="me-2"><i class="fa-solid fa-eye"></i></i></div>
-                            <div><p>${news.total_view}</p></div>
+                        <div class="d-md-flex">
+                            <div class="me-2 text-center"><i class="fa-solid fa-eye"></i></i></div>
+                            <div><p>${news.total_view ? news.total_view : 'No View'}</p></div>
                         </div>
                     </div>
-                        <div class="d-flex">
+                        <div class="d-md-flex">
                             <div class="me-2">
-                                <p>${news.rating.number ? news.rating.number : 'Rating Not Found'}</p>
+                                <p class="m-0 text-center">${news.rating.number ? news.rating.number : 'Rating Not Found'}</p>
                             </div>
-                            <div>
+                            <div class="text-center">
                                 <i class="fa-solid fa-star"></i>
                                 <i class="fa-solid fa-star"></i>
                                 <i class="fa-solid fa-star"></i>
-                                <i class="fa-regular fa-star"></i>
-                                <i class="fa-regular fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star-half-stroke"></i>
                             </div>
                         </div>
                 </div>
                 <div class="text-center mt-4">
-                    <button onclick="newsDetailsInfo('${news._id}')" class="btn btn-dark w-100"  data-bs-toggle="modal" data-bs-target="#newsDetails">Read More</button>
+                    <button onclick="newsDetailsInfo('${news._id ? news._id : 'News Id Missing'}')" class="btn btn-dark w-100"  data-bs-toggle="modal" data-bs-target="#newsDetails">Read More</button>
                 </div>
             </div>
         </div>
@@ -78,13 +81,10 @@ const displayCategoryNews = newses => {
         newsContainer.appendChild(newsDiv);
         // For News Count
         const lengthOfNews = newsContainer.childNodes.length;
+        // console.log(lengthOfNews);
+        const lengthOfNewsStr = JSON.stringify(lengthOfNews)
         const newsCounter = document.getElementById('counter');
-        if (lengthOfNews !== 0) {
-            newsCounter.innerText = lengthOfNews;
-        }
-        else {
-            newsContainer.innerText = 0;
-        }
+        newsCounter.innerText = lengthOfNews;
     })
     toggleingSpinner(false)
 }
@@ -96,13 +96,18 @@ const newsDetailsInfo = info => {
     fetch(url)
         .then(res => res.json())
         .then(data => displayNewsDetails(data.data[0]))
+        .catch((error) => console.log(error))
 }
 
 const displayNewsDetails = news => {
-    console.log(news)
+    // console.log(news);
     const modalBodyInfo = document.getElementById('modal-body-info');
     modalBodyInfo.innerHTML = `
-        <img class="img-fluid" src="${news.image_url}">
+    <div>
+        <img class="img-fluid container d-block" src="${news.image_url ? news.image_url : 'Image Missing'}">
+        <p class="text-center fs-4 fw-semibold pt-3">${news.title ? news.title : 'No Title Found'}</p>
+        <p class="px-3 mt-2">${news.details ? news.details : 'Details Info Missing'}</p>
+    </div>
     `;
 }
 
@@ -118,3 +123,4 @@ const toggleingSpinner = isLoading => {
 
 
 loadCategory();
+gettingCategoryDetails(08);
