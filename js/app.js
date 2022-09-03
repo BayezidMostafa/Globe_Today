d298c1380ae8420e56d7f4435f188dd39172cce1
BayezidMostafa <hiperx1@gmail.com>
2022-09-03 17:32:24 +0600

    Responsive Category Navbar Added

--- js/app.js
+++ js/app.js
@@ -6,16 +6,18 @@ const loadCategory = () => {
 }
 
 const displayCatagory = categories => {
-    const categoriesContainer = document.getElementById('category-container');
+    const containerCategory = document.getElementById('container-catagory');
     categories.forEach(category => {
         // console.log(category)
-        const catDiv = document.createElement('div');
-        catDiv.innerHTML = `
-        <a onclick="gettingCategoryDetails(${category.category_id})" type="button" class="text-decoration-none text-dark">${category.category_name}</a>
+        const categoryUl = document.createElement('ul');
+        categoryUl.classList.add('navbar-nav');
+        categoryUl.innerHTML = `
+        <li class="nav-item">
+            <a onclick="gettingCategoryDetails(${category.category_id})" class="nav-link active" aria-current="page" href="#">${category.category_name}</a>
+        </li>
         `;
-        categoriesContainer.appendChild(catDiv);
+        containerCategory.appendChild(categoryUl);   
     });
-    // Start Spinner 
 }
 
 
@@ -37,14 +39,14 @@ const displayCategoryNews = newses => {
         newsDiv.classList.add('mb-5')
         newsDiv.innerHTML = `
         <div class="row g-0 d-flex align-items-center p-4">
-            <div class=" col-md-2">
+            <div class="col-sm-12 col-md-2">
                 <img class="img-fluid" src="${news.thumbnail_url}" class="img-fluid rounded-start" alt="...">
             </div>
             <div class="col-md-10">
                 <div class="card-body">
                     <h5 class="card-title">${news.title}</h5>
-                    <p class="card-text">${news.details.slice(0, 600)}</p>
-                    <div class="d-flex justify-content-between align-content-center">
+                    <p class="card-text">${news.details.slice(0, 500)}...</p>
+                    <div class="d-flex justify-content-between align-items-center">
                         <div class="d-flex">
                             <div>
                                 <img style="width: 50px;" class="img-fluid rounded-circle" src="${news.author.img}">
@@ -59,14 +61,19 @@ const displayCategoryNews = newses => {
                             <div><p>${news.total_view}</p></div>
                         </div>
                         <div class="d-flex">
-                            <i class="fa-solid fa-star"></i>
-                            <i class="fa-solid fa-star"></i>
-                            <i class="fa-solid fa-star"></i>
-                            <i class="fa-regular fa-star"></i>
-                            <i class="fa-regular fa-star"></i>
+                            <div class="me-2">
+                                <p>${news.rating.number}</p>
+                            </div>
+                            <div>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-regular fa-star"></i>
+                                <i class="fa-regular fa-star"></i>
+                            </div>
                         </div>
                         <div>
-                            <button class="border-0 bg-transparent" type="button"><i
+                            <button onclick="newsDetailsInfo('${news._id}')" class="border-0 bg-transparent" type="button" data-bs-toggle="modal" data-bs-target="#newsDetails"><i
                                     class="fa-solid fa-arrow-right"></i></button>
                         </div>
                     </div>
@@ -80,6 +87,23 @@ const displayCategoryNews = newses => {
     toggleingSpinner(false)
 }
 
+
+
+const newsDetailsInfo = info => {
+    const url = `https://openapi.programming-hero.com/api/news/${info}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayNewsDetails(data.data[0]))
+}
+
+const displayNewsDetails = news => {
+    console.log(news)
+    const modalBodyInfo = document.getElementById('modal-body-info');
+    modalBodyInfo.innerHTML = `
+        <img class="img-fluid" src="${news.image_url}">
+    `;
+}
+
 const toggleingSpinner = isLoading => {
     const loadingScene = document.getElementById('loader-scene');
     if(isLoading){
@@ -91,4 +115,4 @@ const toggleingSpinner = isLoading => {
 }
 
 
-loadCategory()
+loadCategory();
